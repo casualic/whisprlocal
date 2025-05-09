a76e196f73d0145557f013619757291f6fb145c7
casualic <57340188+casualic@users.noreply.github.com>
2025-05-08 22:07:04 -0700

    added deleting functionality to library

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,17 @@
-import { useEffect } from 'react';
+import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { useFrameworkReady } from '@/hooks/useFrameworkReady';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function RootLayout() {
   useFrameworkReady();
 
   return (
-    <>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
       <StatusBar style="auto" />
-    </>
+    </GestureHandlerRootView>
   );
 }
